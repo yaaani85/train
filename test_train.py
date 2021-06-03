38095ff5de3c59b173ac29675cba8695cba78e96
yaaani85 <43518351+yaaani85@users.noreply.github.com>
2021-06-03 19:20:20 +0200

    Update test_train.py

--- test_train.py
+++ test_train.py
@@ -2,5 +2,5 @@ from brane_train import train
 
 #local testing
 def test_preprocess():
-  assert train(True, True) == "Model saved succesfully"
+  assert train("lightgbm" "auc", 1, 30000, 0.05, 4095, 0.28, "binary", True, True) == "Model saved succesfully"
   
